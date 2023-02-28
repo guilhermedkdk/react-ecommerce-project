d1cf28e4ad0b921a07c96aab65f3c66943b95cfe
guilhermedkdk <guilhermeperesmm@gmail.com>
2023-02-28 17:42:14 -0300

    updating Home and creating ProductList

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,12 +1,14 @@
 import { ArrowLeftOutlined, ArrowRightOutlined } from '@mui/icons-material'
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
+import { sliderItems } from '../data'
 
 const Container = styled.div`
     width: 100%;
     height: 100vh;
     display: flex;
     position: relative;
+    overflow: hidden;
 `
 
 const Arrow = styled.div`
@@ -25,10 +27,14 @@ const Arrow = styled.div`
     margin: auto;
     cursor: pointer;
     opacity: 0.5;
+    z-index: 2;
 `
 
 const Wrapper = styled.div`
     height: 100%;
+    display: flex;
+    transition: all 1.0s ease;
+    transform: translateX(${(props) => props.slideIndex * -100}vw);
 `
 
 const Slide = styled.div`
@@ -36,6 +42,7 @@ const Slide = styled.div`
     height: 100vh;
     display: flex;
     align-items: center;
+    background-color: #${props=>props.bg};
 `
 
 const ImgContainer = styled.div`
@@ -73,24 +80,37 @@ const Button = styled.button`
 `
 
 export default function Slider() {
+
+    const [slideIndex, setSlideIndex] = useState(0)
+
+    function handleClick(direction) {
+        if(direction === "left") {
+            setSlideIndex(slideIndex > 0 ? slideIndex - 1 : 2)
+        } else {
+            setSlideIndex(slideIndex < 2 ? slideIndex + 1 : 0)
+        }
+    }
+
   return (
     <Container>
-        <Arrow direction="left">
+        <Arrow direction="left" onClick={()=>handleClick("left")}>
             <ArrowLeftOutlined />
         </Arrow>
-        <Wrapper>
-            <Slide>
+        <Wrapper slideIndex={slideIndex}>
+            {sliderItems.map((item) => (
+            <Slide bg={item.bg} key={item.id}>
                 <ImgContainer>
-                    <Image src={process.env.PUBLIC_URL + "/images/model-ecommerce-0.jpeg"}/>
+                    <Image src={item.img}/>
                 </ImgContainer>
                 <InfoContainer>
-                    <Title>SUMMER SALE</Title>
-                    <Desc>DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.</Desc>
+                    <Title>{item.title}</Title>
+                    <Desc>{item.desc}</Desc>
                     <Button>SHOW NOW</Button>
                 </InfoContainer>
             </Slide>
+            ))}
         </Wrapper>
-        <Arrow direction="right">
+        <Arrow direction="right" onClick={()=>handleClick("right")}>
             <ArrowRightOutlined />
         </Arrow>
     </Container>
